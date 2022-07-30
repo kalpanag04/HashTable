ab2ae45eddd58db5e2415b9014994389676c8f19
kalpanag04 <kalpanag808@gmail.com>
2022-07-30 22:19:14 +0530

    Adding Frequency of words in HashTable

--- Program.cs
+++ Program.cs
@@ -13,6 +13,7 @@ public class Program
         hash.Add("3", "not");
         hash.Add("4", "to");
         hash.Add("5", "be");
+        
         string hash5 = hash.Get("5");
         Console.WriteLine("5th index value: " + hash5);
         string hash2 = hash.Get("2");
